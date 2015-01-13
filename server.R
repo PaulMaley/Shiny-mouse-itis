80154262d926e265cc110b6c1558598e6761d965
Paul Maley <maleyenator@gmail.com>
2015-01-13 14:50:07 +0100

    Implement some basic input and output

--- server.R
+++ server.R
@@ -1,8 +1,70 @@
 library(shiny)
+library(ggplot2)
+library(reshape2)
+
+fl <- seq(0,100)    # frustration levels
+sd_frust <- 10.0   # std deviation of frustration (equal for both populations)
+
 
 shinyServer(function(input, output) {
   
-  output$hdr <- renderText({
-    input$hdr
+  # React to changes in model inputs
+  non_sufferer_mean_frust <- reactive({input$non_sufferer_mean_frust})
+  sufferer_mean_frust <- reactive({input$sufferer_mean_frust})
+  pct_occurence <- reactive({input$pct_occurence})
+  
+  # Frustration model data for plots
+  model_data <- reactive({df<- data.frame(level=fl, 
+                                          suff_occur = (100 - pct_occurence()) * dnorm((fl-sufferer_mean_frust())/sd_frust),
+                                          non_suff_occ = pct_occurence() * dnorm((fl-non_sufferer_mean_frust())/sd_frust)
+                                         )
+                          melt(df, c("level"),  variable.name=c("SubPopulation"), value.name=c("Occurence"))
+  })
+  
+  output$frustration <- renderPlot({
+    ggplot(model_data()) + geom_area(aes(x=level, y=Occurence, group=SubPopulation, fill=SubPopulation), alpha=0.5) +
+                           xlab("Frustration level")
+  })
+
+  # Generate data sample
+  n_samples <- reactive({input$n})
+
+  # Generate data sample only when the button is pushed
+  # not when the number of samples is changed
+  sample_data <- reactive({
+    input$generate
+    n <- isolate(n_samples())
+    # cat("Generate: ", n, "\n", sep="")
+    
+    # Random vector of Afflicted/Not afflicted
+    df = data.frame(Afflicted=sample(c(TRUE,FALSE), n, replace=TRUE, 
+                                     prob=c(isolate(pct_occurence()/100.), 1 - isolate(pct_occurence()/100.) ))
+                    )
+    
+    # Get means for afflicted and unaflicted people
+    smf <- isolate(sufferer_mean_frust())
+    nsmf <- isolate(non_sufferer_mean_frust())
+    
+    # Determine random level of frustration dependent on affliction status
+    df$FrustrationLevel <- rnorm(n, ifelse(df$Afflicted, smf, nsmf), sd_frust)
+
+    # Get probabilities
+    P_TP <- isolate(reactive({input$test_true_positive/100.}))
+    P_FP <- isolate(reactive({input$test_false_positive/100.}))
+  
+    # Determine random test result also dependent of affliction status
+    df$TestResult <- sapply(df$Afflicted, function(x) { ifelse(x,
+                                            sample(c("Positive","Negative"), 1, prob=c(P_TP(), 1-P_TP())),
+                                            sample(c("Positive","Negative"), 1, prob=c(P_FP(), 1-P_FP())))})
+    # print(df)
+    df
+  })
+
+  # Render the data frame in a table 
+  output$samples <- renderTable({ sample_data() })
+
+  # Display the sample data graphically
+  output$samples_graphic <- renderPlot({
+    ggplot(sample_data()) + geom_point(aes(x=FrustrationLevel, y=Afflicted, color=TestResult))
   })
 })
--- ui.R
+++ ui.R
@@ -4,16 +4,60 @@ library(shiny)
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("Placeholder App"),
+  titlePanel("Mouse-itis predictor: An exercise in Shiny"),
   
   sidebarLayout(
     sidebarPanel(
-      h3("Sidebar"),
-      textInput("hdr", label="Enter header")
+      
+      
+      h3("The model"),
+      p("Mouse-itis is a neurological condition found in a 
+         small fraction of the population. It's principle
+         symptom is an increased level of frustration (measured
+         on a scale from 0 to 100) which however due to differences
+         in inherent levels of frustration is not a binary indicator.
+         Fortunately the condition is detectable by a test but 
+         not surprisingly this test is
+         not completely reliable, giving occasional false 
+         positives and missing the occasional sufferer."),
+      sliderInput("pct_occurence", 
+                  label="Occurence of the condition in the population (%)",
+                  min=1, max=10, value=2),
+      br(),
+      sliderInput("non_sufferer_mean_frust",
+                  label="Mean frustration of non-sufferer",
+                  min=30, max=50, value=40),
+      br(),
+      sliderInput("sufferer_mean_frust",
+                  label="Mean frustration of non-sufferer",
+                  min=50, max=70, value=60),
+      br(),
+      
+      
+      h3("Some samples"),
+      numericInput("n", label="Number of samples to generate", 10,
+                   min=10, max=100),
+      br(),
+      actionButton("generate", label="Generate samples"),
+      
+      h3("The test"),
+      sliderInput("test_false_positive", 
+                  label="Probability (false positive) (%)",
+                  min=0, max=5, value=1),
+      br(),
+      sliderInput("test_true_positive", 
+                  label="Probability (true positive) (%)",
+                  min=90, max=100, value=95),
+      br()
     ),
     
     mainPanel(
-      h3(textOutput("hdr"), align="center")
+      h3("Visualization of condition"),
+      plotOutput("frustration"),
+      br(),
+      h3("Generated data"),
+      plotOutput("samples_graphic"),
+      tableOutput("samples")
     )
   )
 ))
